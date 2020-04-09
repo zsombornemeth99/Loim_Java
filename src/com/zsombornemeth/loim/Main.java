54770133cbf1e63ea78492ecd45d939fcc49a220
zsombornemeth99 <zsombor.nemeth99@gmail.com>
2020-04-09 14:11:51 +0200

    Kerdesek osztály majdnem kész

--- src/com/zsombornemeth/loim/Main.java
+++ src/com/zsombornemeth/loim/Main.java
@@ -1,10 +1,16 @@
 package com.zsombornemeth.loim;
 
+import java.util.Random;
+
 public class Main
 {
+    public static Random rnd = new Random();
 
     public static void main(String[] args)
     {
         SorKerdesek sk = new SorKerdesek("sorkerdes.txt");
+
+        Kerdesek k = new Kerdesek("kerdes.txt");
+        System.out.println(k.getKerdesekSzama(15));
     }
 }
